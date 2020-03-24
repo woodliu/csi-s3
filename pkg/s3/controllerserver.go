d6d51083472a717e8a1905d4177e83aeeff2b945
liuchanglin <liuchanglin@csvw.com>
2020-03-24 23:37:37 +0800

    change spec path

--- pkg/s3/controllerserver.go
+++ pkg/s3/controllerserver.go
@@ -29,7 +29,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/woodliu/csi-s3/pkg/container-storage-interface/spec/lib/go/csi/v0"
 	"github.com/kubernetes-csi/drivers/pkg/csi-common"
 )
 
