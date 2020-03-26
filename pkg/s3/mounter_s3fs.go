1d5c2bbf015cf26aa8e3bb511d29287b8e19aaf3
liuchanglin <liuchanglin@csvw.com>
2020-03-26 21:06:26 +0800

    update s3fs arg

--- pkg/s3/mounter_s3fs.go
+++ pkg/s3/mounter_s3fs.go
@@ -43,7 +43,6 @@ func (s3fs *s3fsMounter) Mount(source string, target string) error {
 		fmt.Sprintf("%s", target),
 		"-o", "use_path_request_style",
 		"-o", fmt.Sprintf("url=%s", s3fs.url),
-		"-o", fmt.Sprintf("endpoint=%s", s3fs.region),
 		"-o", "allow_other",
 		"-o", "mp_umask=000",
 	}
